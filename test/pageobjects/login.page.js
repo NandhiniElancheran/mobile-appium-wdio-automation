617ca96c4fd92970d4ddc5c2e1c63b1ada47bcda
Nandhini Elancheran <nandhinivikna@gmail.com>
2023-02-24 10:32:05 +0400

    Stripe test script changes

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,15 +1,29 @@
 
 const loginPageSelectors = {
-    nextBtn : ("//android.widget.TextView[@text='Next']"),
-    phoneNumberTxt: "//android.widget.EditText",
-   };
+  nextBtn: ("//android.widget.TextView[@text='Next']"),
+  phoneNumberTxt: "//android.widget.EditText",
+  countryCodeTxt: "//android.widget.TextView[@text='Country code']",
+  searchEditTxt: "//android.widget.EditText[@text='Search']",
+  
+};
 
-class LoginPage  {
-    
-    async login (mobileNumber) {
-     await $(loginPageSelectors.phoneNumberTxt).setValue(mobileNumber);
-     await $(loginPageSelectors.nextBtn).click();
-    }
+class LoginPage {
+
+  async login(mobileNumber, country) {
+    await $(loginPageSelectors.countryCodeTxt).click();
+    await new Promise(r => setTimeout(r, 5000));
+    await $(loginPageSelectors.searchEditTxt).setValue(country);
+    await $(`//android.widget.TextView[@text='${country}']`).click();
+    await $(loginPageSelectors.phoneNumberTxt).setValue(mobileNumber);
+   // await new Promise(r => setTimeout(r, 5000));
+    let elem = $(loginPageSelectors.nextBtn)
+    let isEnabled = elem.isDisplayed();
+   // if(!isEnabled){
+      await $(loginPageSelectors.phoneNumberTxt).clearValue();
+      await $(loginPageSelectors.phoneNumberTxt).setValue(mobileNumber);
+ //   }
+      await $(loginPageSelectors.nextBtn).click();
+  }
 
 }
 
--- test/pageobjects/top_up.page.js
+++ test/pageobjects/top_up.page.js
@@ -2,10 +2,12 @@
 // import { CONTEXT_REF } from "../helper/WebView";
 //const WebView = require('../helper/WebView.js');
 //const CONTEXT_REF = require('../helper/WebView.js');
+const { setTimeout } = require('timers/promises');
+
 
 const topUpPageSelectors = {
     debitCardTxt: "//android.widget.TextView[@text='Debit Card']",
-    amountTxt: "//android.widget.EditText[@text='20']",
+    amountTxt: "(//android.widget.EditText)[1]",
     cardNumberTxt: "//android.widget.EditText[@text='Card number']",
     cardExpiryDate: "//android.widget.EditText[@text='MM/YY']",
     cardCvv: "//android.widget.EditText[@text='CVV']",
@@ -15,7 +17,10 @@ const topUpPageSelectors = {
     closeTransactioneCompleteBtn: "//android.widget.TextView[@text='Close']",
     statusTxt: "//android.widget.TextView[@text='Confirmed']",
     //closeBtn: "addMoneyFinishedButton",
-    webViewContent: "//android.webkit.WebView//android.view.View[3]"
+    webViewContent: "//android.webkit.WebView//android.view.View[3]",
+    stripeCardNumberTxt: '//android.widget.LinearLayout[@content-desc="Card number"]/android.widget.FrameLayout/android.widget.EditText',
+    stripeDateExpiryTxt: '//android.widget.LinearLayout[@content-desc="Expiration date"]/android.widget.FrameLayout/android.widget.EditText',
+    stripeCvv: '//android.widget.LinearLayout[@content-desc="CVC"]/android.widget.FrameLayout/android.widget.EditText',
 };
 const CONTEXT_REF = {
     NATIVE: 'native_app',
@@ -28,17 +33,39 @@ const DOCUMENT_READY_STATE = {
 };
 class TopUpPage {
 
-    async selectDebit(amount, cardNumber, expiryDate, cvv) {
+    async selectDebit() {
         await $(topUpPageSelectors.debitCardTxt).click();
+    }
+    async enterCheckoutDebitCardDetails(amount, cardNumber, expiryDate, cvv){
         await $(topUpPageSelectors.amountTxt).setValue(amount);
         await $(topUpPageSelectors.cardNumberTxt).setValue(cardNumber);
         await $(topUpPageSelectors.cardExpiryDate).setValue(expiryDate);
         await $(topUpPageSelectors.cardCvv).setValue(cvv);
         await $(topUpPageSelectors.topUpBtn).click();
     }
+    
+    async enterStripeDebitCardDetails(amount, cardNumber, expiryDate, cvv){
+        await $(topUpPageSelectors.amountTxt).setValue(amount);
+        await $(topUpPageSelectors.stripeCardNumberTxt).setValue(cardNumber);
+        await $(topUpPageSelectors.stripeDateExpiryTxt).setValue(expiryDate);
+        await $(topUpPageSelectors.stripeCvv).setValue(cvv);
+        let elem = $(topUpPageSelectors.topUpBtn)
+        let isEnabled = elem.isEnabled();
+        console.log(isEnabled);
+      //  if(!isEnabled){
+            await setTimeout (done(),3000);
+     //   }
+        await $(topUpPageSelectors.topUpBtn).click();
+        await setTimeout (done(),10000);
+    }
+
     async verifyTopUpDetails() {
+       // driver.pause(10000);
+       // await new Promise(r => setTimeout(r, 10000));
+        await setTimeout (3000);
         const el = await $(topUpPageSelectors.bottomSheet);
         await el.waitForDisplayed({ timeout: 10000 });
+        await setTimeout (3000);
         browser.back();
         // browser.action('pointer'. {
         //     parameters: { pointerType: 'mouse' } // "mouse" is default value, also possible: "pen" or "touch"
@@ -57,15 +84,19 @@ class TopUpPage {
         //     await this.switchToContext(CONTEXT_REF.NATIVE);
     }
     async verifyTransactionDetails() { 
-        driver.pause(3000);
+       // driver.pause(10000);
+       // await new Promise(r => setTimeout(r, 10000));
+        await setTimeout (done,10000);
         const elem = await $(topUpPageSelectors.transactionCompleteTitle);
         await expect(elem).toBeDisplayed();
+        await setTimeout (done,10000);
         await $(topUpPageSelectors.closeTransactioneCompleteBtn).click();
         //    await $(topUpPageSelectors.transactionCompleteTitle).toHaveText('Transaction completed');
         //    await $(topUpPageSelectors.statusTxt).toHaveText('Confirmed');
     }
 
     async navigateToTopUpOptionsPage(){
+        await setTimeout (done,10000);
         browser.back();
     }
 
